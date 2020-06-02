1b5cd91d658c43fff1c7294a3784f5e38bf8360a
jin <0201xFy0201>
2020-06-02 18:09:51 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -66,7 +66,7 @@ export const constantRoutes = [
         path: 'table',
         name: 'Table',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        meta: { title: 'TableTest001', icon: 'table' }
       },
       {
         path: 'tree',
